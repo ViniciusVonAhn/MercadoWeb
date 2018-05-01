552b764c10d43a4847b43bf143366ffe417bead3
Fabricio Mota <ftlord@gmail.com>
2018-05-01 16:42:23 -0300

    Anotações de Validação

--- src/main/java/local/model/Cupom.java
+++ src/main/java/local/model/Cupom.java
@@ -5,6 +5,10 @@ import java.sql.Time;
 import java.util.List;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 
 @Entity
@@ -16,30 +20,48 @@ public class Cupom{
             @AttributeOverride(name = "produto_id", column = @Column(name = "produto_id", nullable = false)) })
 	private CupomID id;
 	
+	@NotBlank(message = "A Data deve ser informado!")
+	@NotNull(message = "A Data não pode ficar em branco!")
+	//@Column(name = "nome", length = 50, nullable = false)
 	@Column(nullable=false)
 	private Date data;
 	
+	@NotBlank(message = "A Hora deve ser informado!")
+	@NotNull(message = "A Hora não pode ficar em branco!")
 	@Column(nullable=false)
 	private Time hora;
 	
-	@Column(name="nome_produto", nullable=true)
+	@NotBlank(message = "O Nome do produto deve ser informado!")
+	@NotNull(message = "O Nome do produto não pode ficar em branco!")
+	@Column(name="nome_produto", length = 50, nullable=true)
 	private String nomeProduto;
 	
-	@Column(nullable=true)
+	
+	@Column(length = 14)
 	private String cpf;
 	
+	@NotBlank(message = "A Quantidade Vendida deve ser informado!")
+	@NotNull(message = "A Quantidade Vendida não pode ficar em branco!")
 	@Column(name="quant_vendida", nullable=false, columnDefinition="Decimal(9,2)")
 	private double quantVendida;
 	
+	@NotBlank(message = "O Valor Unitário deve ser informado!")
+	@NotNull(message = "O Valor Unitário não pode ficar em branco!")
 	@Column(name="valor_unitario", nullable=false, columnDefinition="Decimal(9,2)")
 	private double valorUnitario;
 	
+	@NotBlank(message = "O Valor do Produto deve ser informado!")
+	@NotNull(message = "O Valor do Produto não pode ficar em branco!")
 	@Column(name="valor_produto", nullable=false, columnDefinition="Decimal(9,2)")
 	private double valorProduto;
 	
+	@NotBlank(message = "O Valor Recebido deve ser informado!")
+	@NotNull(message = "O Valor Recebido não pode ficar em branco!")
 	@Column(name="valor_recebido", nullable=false, columnDefinition="Decimal(9,2)")
 	private double valorRecebido;
 	
+	@NotBlank(message = "O Valor do Troco deve ser informado!")
+	@NotNull(message = "O Valor do Troco não pode ficar em branco!")
 	@Column(name="valor_troco", nullable=false, columnDefinition="Decimal(9,2)")
 	private double valorTroco;
 	
@@ -51,6 +73,7 @@ public class Cupom{
 	@JoinColumn(name="empresa_id", nullable=false)
 	private Empresa empresa;
 	
+	@JsonIgnore
 	@OneToMany(mappedBy = "cupom")
 	private List<Venda> venda;
 
@@ -157,7 +180,101 @@ public class Cupom{
 	public void setVenda(List<Venda> venda) {
 		this.venda = venda;
 	}
-	
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+		result = prime * result + ((data == null) ? 0 : data.hashCode());
+		result = prime * result + ((empresa == null) ? 0 : empresa.hashCode());
+		result = prime * result + ((hora == null) ? 0 : hora.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nomeProduto == null) ? 0 : nomeProduto.hashCode());
+		result = prime * result + ((produto == null) ? 0 : produto.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(quantVendida);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(valorProduto);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(valorRecebido);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(valorTroco);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(valorUnitario);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((venda == null) ? 0 : venda.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cupom other = (Cupom) obj;
+		if (cpf == null) {
+			if (other.cpf != null)
+				return false;
+		} else if (!cpf.equals(other.cpf))
+			return false;
+		if (data == null) {
+			if (other.data != null)
+				return false;
+		} else if (!data.equals(other.data))
+			return false;
+		if (empresa == null) {
+			if (other.empresa != null)
+				return false;
+		} else if (!empresa.equals(other.empresa))
+			return false;
+		if (hora == null) {
+			if (other.hora != null)
+				return false;
+		} else if (!hora.equals(other.hora))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nomeProduto == null) {
+			if (other.nomeProduto != null)
+				return false;
+		} else if (!nomeProduto.equals(other.nomeProduto))
+			return false;
+		if (produto == null) {
+			if (other.produto != null)
+				return false;
+		} else if (!produto.equals(other.produto))
+			return false;
+		if (Double.doubleToLongBits(quantVendida) != Double.doubleToLongBits(other.quantVendida))
+			return false;
+		if (Double.doubleToLongBits(valorProduto) != Double.doubleToLongBits(other.valorProduto))
+			return false;
+		if (Double.doubleToLongBits(valorRecebido) != Double.doubleToLongBits(other.valorRecebido))
+			return false;
+		if (Double.doubleToLongBits(valorTroco) != Double.doubleToLongBits(other.valorTroco))
+			return false;
+		if (Double.doubleToLongBits(valorUnitario) != Double.doubleToLongBits(other.valorUnitario))
+			return false;
+		if (venda == null) {
+			if (other.venda != null)
+				return false;
+		} else if (!venda.equals(other.venda))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Cupom [id=" + id + ", data=" + data + ", hora=" + hora + ", nomeProduto=" + nomeProduto + ", cpf=" + cpf
+				+ ", quantVendida=" + quantVendida + ", valorUnitario=" + valorUnitario + ", valorProduto="
+				+ valorProduto + ", valorRecebido=" + valorRecebido + ", valorTroco=" + valorTroco + ", produto="
+				+ produto + ", empresa=" + empresa + "]";
+	}
 	
 }
--- src/main/java/local/model/Produto.java
+++ src/main/java/local/model/Produto.java
@@ -3,6 +3,8 @@ package local.model;
 import java.util.List;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -14,15 +16,21 @@ public class Produto{
 	@Column(name = "produto_id", unique = true, nullable = false)
 	private int produto_id;
 	
-	@Column(nullable=false)
+	@NotBlank(message = "O Nome deve ser informado!")
+	@NotNull(message = "O Nome não pode ficar em branco!")
+	@Column(name = "nome", length = 50, nullable = false)
 	private String nome;
 	
-	@Column(name="codigo_de_barra")
+	@Column(name="codigo_de_barra", length = 13)
 	private String codigoDeBarra;
 	
+	@NotBlank(message = "A quantidade em estoque deve ser informado!")
+	@NotNull(message = "A quantidade em estoque não pode ficar em branco!")
 	@Column(nullable=false)
 	private int estoque;
 	
+	@NotBlank(message = "O valor Unitário deve ser informado!")
+	@NotNull(message = "O valor Unitário não pode ficar em branco!")
 	@Column(name="valor_un", nullable=false, columnDefinition="Decimal(9,2)")
 	private double valorUn;
 
@@ -78,7 +86,58 @@ public class Produto{
 		this.cupom = cupom;
 	}
 
-	
-	
-	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigoDeBarra == null) ? 0 : codigoDeBarra.hashCode());
+		result = prime * result + ((cupom == null) ? 0 : cupom.hashCode());
+		result = prime * result + estoque;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + produto_id;
+		long temp;
+		temp = Double.doubleToLongBits(valorUn);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		if (codigoDeBarra == null) {
+			if (other.codigoDeBarra != null)
+				return false;
+		} else if (!codigoDeBarra.equals(other.codigoDeBarra))
+			return false;
+		if (cupom == null) {
+			if (other.cupom != null)
+				return false;
+		} else if (!cupom.equals(other.cupom))
+			return false;
+		if (estoque != other.estoque)
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (produto_id != other.produto_id)
+			return false;
+		if (Double.doubleToLongBits(valorUn) != Double.doubleToLongBits(other.valorUn))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Produto [produto_id=" + produto_id + ", nome=" + nome + ", codigoDeBarra=" + codigoDeBarra
+				+ ", estoque=" + estoque + ", valorUn=" + valorUn + "]";
+	}
+
 }
